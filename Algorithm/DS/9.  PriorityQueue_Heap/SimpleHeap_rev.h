898871863d0201eb799779bac906b2bf9c0afdc6
donghunp <donghunp1212>
2019-10-14 09:51:50 +0900

    add DS

---
+++ Algorithm/DS/9.  PriorityQueue_Heap/SimpleHeap_rev.h
@@ -0,0 +1,38 @@
+#ifndef __SIMPLE_HEAP_H__
+#define __SIMPLE_HEAP_H__
+
+#define True 1
+#define False 0
+
+#define HEAP_LEN 100
+
+typedef char HData;
+//typedef int Priority;
+typedef int PriorityComp(HData d1, HData d2);
+
+// typedef struct __heapElem{
+//     Priority pr;
+//     HData data;
+// } HeapElem;
+
+// typedef struct _heap{
+//     int numOfData;
+//     HeapElem heapArr[HEAP_LEN];
+// } Heap;
+
+typedef struct _heap{
+    PriorityComp *cmp;
+    int numOfData;
+    HData heapArr[HEAP_LEN];
+} Heap;
+
+void HeapInit(Heap *ph, PriorityComp pc);
+
+int HIsEmpty(Heap *ph);
+
+//void HInsert(Heap * ph, HData data, Priority pr);
+void HInsert(Heap * ph, HData data);
+
+HData HDelete(Heap *ph);
+
+#endif
